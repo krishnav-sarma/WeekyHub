409bd8ea15d21d3d3c3bc2834b2b3b2019d1edde
krishnav <k.sarmah666@gmail.com>
2025-09-10 22:41:38 +0530

    sent part font

--- src/pages/Requestpage.jsx
+++ src/pages/Requestpage.jsx
@@ -28,14 +28,14 @@ const Requestpage = () => {
   return (
     <div>
       <div className="p-4 max-w-screen mx-auto bg-[#18181b] ">
-        <h1 className="text-[#d7d2cf] text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold mt-10 ml-2">
+        <h1 className="text-[#d7d2cf] text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-f2 mt-15 ml-2">
           Sent Request{" "}
         </h1>
         <form onSubmit={hundleSubmit}>
           <input name="name" placeholder="Movie name" className="w-full px-4 py-3 rounded-xl bg-[#2a2a2d] text-[#d7d2cf] placeholder-gray-400 
                  focus:outline-none focus:ring-2 focus:ring-[#d7d2cf] transition mt-4.5"  /> <br />
-          <button className="w-full py-3 bg-[#d7d2cf] text-[#18181b] font-semibold rounded-xl 
-                 hover:bg-[#bdb9b6] transition shadow-md mt-4.5">Sent</button>
+          <button className="w-full py-3 bg-[#d7d2cf] text-[#18181b]  rounded-xl text-xl
+                 hover:bg-[#bdb9b6] transition shadow-md mt-4.5 font-f2">Sent</button>
         </form>
       </div>
       <Requestmovies />
